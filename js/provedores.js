6bdf492c57deee4cdeb0632b46be382b1a9ab983
mokerony <169068390+mokerony@users.noreply.github.com>
2024-09-27 21:26:02 -0500

    Update provedores.js

--- js/provedores.js
+++ js/provedores.js
@@ -1,7 +1,7 @@
 document.querySelector('#boton').addEventListener('click', traerDatos);
 function traerDatos(){
     const xhttp = new XMLHttpRequest();
-    xhttp.open('GET', '../JSON/provedores.json',true)
+    xhttp.open('GET', '../json/provedores.json',true)
     xhttp.send();
     xhttp.onreadystatechange = function(){
         if(this.readyState==4 && this.status == 200){
