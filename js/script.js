829999cf043d3bc8ef7f2f3cd3ff37c57a6fa9aa
mokerony <169068390+mokerony@users.noreply.github.com>
2024-09-27 22:46:54 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -41,266 +41,265 @@ const productos = [
         precio: "$3.000"
     },
     {
-    id: "jugo de corozo",
-    titulo: "jugo de corozo",
-    imagen: "../img/bebidas/jugo-corozo.jpg",
-    categoria: {
-        nombre: "bebidas",
-        id: "bebidas"
+        id: "jugo de corozo",
+        titulo: "jugo de corozo",
+        imagen: "../img/bebidas/jugo-corozo.jpg",
+        categoria: {
+            nombre: "bebidas",
+            id: "bebidas"
+        },
+        precio: "$3.000"
+    }, 
+    {
+        id: "barrilete",
+        titulo: "barrilete",
+        imagen: "../img/dulces/barrilete.png",
+        categoria: {
+            nombre: "dulces",
+            id: "dulces"
+        },
+        precio: "$800"
     },
-    precio: "$3.000"
-}, {
-    id: "barrilete",
-    titulo: "barrilete",
-    imagen: "../img/dulces/barrilete.png",
-    categoria: {
-        nombre: "dulces",
-        id: "dulces"
+    {
+        id: "bombon",
+        titulo: "bombon",
+        imagen: "../img/dulces/bonbon.jpeg",
+        categoria: {
+            nombre: "dulces",
+            id: "dulces"
+        },
+        precio: "$1.000"
     },
-    precio: "$800"
-},
-{
-    id: "bombon",
-    titulo: "bombon",
-    imagen: "../img/dulces/bonbon.jpeg",
-    categoria: {
-        nombre: "dulces",
-        id: "dulces"
+    {
+        id: "candi ranch",
+        titulo: "candi ranch",
+        imagen: "../img/dulces/candi-ranch.jpeg",
+        categoria: {
+            nombre: "dulces",
+            id: "dulces"
+        },
+        precio: "$1.000"
     },
-    precio:"$1.000"
-},
-{
-    id: "candi ranch",
-    titulo: "candi ranch",
-    imagen: "../img/dulces/candi-ranch.jpeg",
-    categoria: {
-        nombre: "dulces",
-        id: "dulces"
+    {
+        id: "chocomelo",
+        titulo: "chocomelo",
+        imagen: "../img/dulces/chocomelo.jpeg",
+        categoria: {
+            nombre: "dulces",
+            id: "dulces"
+        },
+        precio: "$2.000"
     },
-    precio: "$1.000"
-},
-{
-    id: "chocomelo",
-    titulo: "chocomelo",
-    imagen: "../img/dulces/chocomelo.jpeg",
-    categoria: {
-        nombre: "dulces",
-        id: "dulces"
+    {
+        id: "jet",
+        titulo: "jet",
+        imagen: "../img/dulces/jet.jpeg",
+        categoria: {
+            nombre: "dulces",
+            id: "dulces"
+        },
+        precio: "$800"
     },
-    precio: "$2.000"
-},
-{
-    id: "jet",
-    titulo: "jet",
-    imagen: "../img/dulces/jet.jpeg",
-    categoria: {
-        nombre: "dulces",
-        id: "dulces"
+    {
+        id: "jumbo",
+        titulo: "jumbo",
+        imagen: "../img/dulces/jumbo.jpeg",
+        categoria: {
+            nombre: "dulces",
+            id: "dulces"
+        },
+        precio: "$800"
     },
-    precio: "$800"
-},
-{
-    id: "jumbo",
-    titulo: "jumbo",
-    imagen: "../img/dulces/jumbo.jpeg",
-    categoria: {
-        nombre: "dulces",
-        id: "dulces"
+    {
+        id: "manimoto",
+        titulo: "manimoto",
+        imagen: "../img/dulces/manimoto.png",
+        categoria: {
+            nombre: "dulces",
+            id: "dulces"
+        },
+        precio: "$1.500"
     },
-    precio: "$800"
-},
-{
-    id: "manimoto",
-    titulo: "manimoto",
-    imagen: "../img/dulces/manimoto.png",
-    categoria: {
-        nombre: "dulces",
-        id: "dulces"
+    {
+        id: "menta",
+        titulo: "menta",
+        imagen: "../img/dulces/menta.jpg",
+        categoria: {
+            nombre: "dulces",
+            id: "dulces"
+        },
+        precio: "$200"
     },
-    precio: "$1.500"
-},{
-    id: "menta",
-    titulo: "menta",
-    imagen: "../img/dulces/menta.jpg",
-    categoria: {
-        nombre: "dulces",
-        id: "dulces"
+    {
+        id: "nucita",
+        titulo: "nucita",
+        imagen: "../img/dulces/nucita.png",
+        categoria: {
+            nombre: "dulces",
+            id: "dulces"
+        },
+        precio: "$200"
     },
-    precio: "$200"
-},
-{
-    id: "nucita",
-    titulo: "nucita",
-    imagen: "../img/dulces/nucita.png",
-    categoria: {
-        nombre: "dulces",
-        id: "dulces"
+    {
+        id: "quipitos",
+        titulo: "quipitos",
+        imagen: "../img/dulces/quipitos.png",
+        categoria: {
+            nombre: "dulces",
+            id: "dulces"
+        },
+        precio: "$600"
     },
-    precio: "$200"
-},
-{
-    id: "quipitos",
-    titulo: "quipitos",
-    imagen: "../img/dulces/quipitos.png",
-    categoria: {
-        nombre: "dulces",
-        id: "dulces"
+    {
+        id: "empanada",
+        titulo: "empanada",
+        imagen: "../img/horneados/empanadas-horneadas.png",
+        categoria: {
+            nombre: "horneados",
+            id: "horneados"
+        },
+        precio: "$2.000"
     },
-    precio: "$600"
-},
-{
-    id: "empanada",
-    titulo: "empanada",
-    imagen: "../img/horneados/empanadas-horneadas.png",
-    categoria: {
-        nombre: "horneados",
-        id: "horneados"
+    {
+        id: "dedito",
+        titulo: "dedito",
+        imagen: "../img/horneados/deditos.png",
+        categoria: {
+            nombre: "horneados",
+            id: "horneados"
+        },
+        precio: "$2.000"
     },
-    precio: "$2.000"
-},
-{
-    id: "dedito",
-    titulo: "dedito",
-    imagen: "../img/horneados/deditos.png",
-    categoria: {
-        nombre: "horneados",
-        id: "horneados"
+    {
+        id: "pastelitos",
+        titulo: "pastelitos",
+        imagen: "../img/horneados/pastelitos.jpg",
+        categoria: {
+            nombre: "horneados",
+            id: "horneados"
+        },
+        precio: "$2.000"
     },
-    precio: "$2.000"
-},
-{
-    id: "pastelitos",
-    titulo: "pastelitos",
-    imagen: "../img/horneados/pastelitos.jpg",
-    categoria: {
-        nombre: "horneados",
-        id: "horneados"
+    {
+        id: "sandwich",
+        titulo: "sandwich",
+        imagen: "../img/horneados/sandwich.jpg",
+        categoria: {
+            nombre: "horneados",
+            id: "horneados"
+        },
+        precio: "$1.500"
     },
-    precio: "$2.000"
-},
-{
-    id: "sandwhich",
-    titulo: "sandwich",
-    imagen: "../img/horneados/sandwich.jpg",
-    categoria: {
-        nombre: "horneados",
-        id: "horneados"
+    {
+        id: "chestres",
+        titulo: "chestres",
+        imagen: "../img/mecatos/chestres.png",
+        categoria: {
+            nombre: "mecatos",
+            id: "mecatos"
+        },
+        precio: "$1.000"
     },
-    precio: "$1.500"
-},
-{
-    id: "chestres",
-    titulo: "chestres",
-    imagen: "../img/mecatos/chestres.png",
-    categoria: {
-        nombre: "mecatos",
-        id: "mecatos"
+    {
+        id: "choclitos",
+        titulo: "choclitos",
+        imagen: "../img/mecatos/choclitos.jpeg",
+        categoria: {
+            nombre: "mecatos",
+            id: "mecatos"
+        },
+        precio: "$2.000"
     },
-    precio: "$1.000"
-},
-{
-    id: "choclitos",
-    titulo: "choclitos",
-    imagen: "../img/mecatos/choclitos.jpeg",
-    categoria: {
-        nombre: "mecatos",
-        id: "mecatos"
+    {
+        id: "detodito mix",
+        titulo: "detodito mix",
+        imagen: "../img/mecatos/detodito-mix.jpeg",
+        categoria: {
+            nombre: "mecatos",
+            id: "mecatos"
+        },
+        precio: "$3.000"
     },
-    precio: "$2.000"
-},
-{
-    id: "detodito mix",
-    titulo: "detodito mix",
-    imagen: "../img/mecatos/detodito-mix.jpeg",
-    categoria: {
-        nombre: "mecatos",
-        id: "mecatos"
+    {
+        id: "detodito natural",
+        titulo: "detodito natural",
+        imagen: "../img/mecatos/detodito-natural.jpg",
+        categoria: {
+            nombre: "mecatos",
+            id: "mecatos"
+        },
+        precio: "$3.000"
     },
-    precio: "$3.000"
-},
-{
-    id: "detodito natural",
-    titulo: "detodito natural",
-    imagen: "../img/mecatos/detodito-natural.jpg",
-    categoria: {
-        nombre: "mecatos",
-        id: "mecatos"
+    {
+        id: "detodito picante",
+        titulo: "detodito picante",
+        imagen: "../img/mecatos/detodito-picante.jpeg",
+        categoria: {
+            nombre: "mecatos",
+            id: "mecatos"
+        },
+        precio: "$3.000"
     },
-    precio: "$3.000"
-},
-{
-    id: "detodito picante",
-    titulo: "detodito picante",
-    imagen: "../img/mecatos/detodito-picante.jpeg",
-    categoria: {
-        nombre: "mecatos",
-        id: "mecatos"
+    {
+        id: "doritos",
+        titulo: "doritos",
+        imagen: "../img/mecatos/doritos.jpeg",
+        categoria: {
+            nombre: "mecatos",
+            id: "mecatos"
+        },
+        precio: "$2.000"
     },
-    precio: "$3.000"
-},
-{
-    id: "doritos",
-    titulo: "doritos",
-    imagen: "../img/mecatos/doritos.jpeg",
-    categoria: {
-        nombre: "mecatos",
-        id: "mecatos"
+    {
+        id: "papas bbq",
+        titulo: "papas bqq",
+        imagen: "../img/mecatos/papas-bbq.jpeg",
+        categoria: {
+            nombre: "mecatos",
+            id: "mecatos"
+        },
+        precio: "$2.000"
     },
-    precio: "$2.000"
-},
-{
-    id: "papas bbq",
-    titulo: "papas bqq",
-    imagen: "../img/mecatos/papas-bbq.jpeg",
-    categoria: {
-        nombre: "mecatos",
-        id: "mecatos"
+    {
+        id: "papas limon",
+        titulo: "papas limon",
+        imagen: "../img/mecatos/papas-limon.jpg",
+        categoria: {
+            nombre: "mecatos",
+            id: "mecatos"
+        },
+        precio: "$2.000"
     },
-    precio: "$2.000"
-},
-{
-    id: "papas limon",
-    titulo: "papas limon",
-    imagen: "../img/mecatos/papas-limon.jpg",
-    categoria: {
-        nombre: "mecatos",
-        id: "mecatos"
+    {
+        id: "papas natural",
+        titulo: "papas natural",
+        imagen: "../img/mecatos/papas-natural.jpg",
+        categoria: {
+            nombre: "mecatos",
+            id: "mecatos"
+        },
+        precio: "$2.000"
     },
-    precio: "$2.000"
-},
-{
-    id: "papas natural",
-    titulo: "papas natural",
-    imagen: "../img/mecatos/papas-natural.jpg",
-    categoria: {
-        nombre: "mecatos",
-        id: "mecatos"
-    },
-    precio: "$2.000"
-},
-{
-    id: "papas pollo",
-    titulo: "papas pollo",
-    imagen: "../img/mecatos/papas-pollo.jpg",
-    categoria: {
-        nombre: "mecatos",
-        id: "mecatos"
-    },
-    precio: "$2.000"
-}
+    {
+        id: "papas pollo",
+        titulo: "papas pollo",
+        imagen: "../img/mecatos/papas-pollo.jpg",
+        categoria: {
+            nombre: "mecatos",
+            id: "mecatos"
+        },
+        precio: "$2.000"
+    }
 ];
 
 const contenedorProductos = document.querySelector("#contenedor-productos");
 const botonesCategorias = document.querySelectorAll(".boton-categoria");
-const  tituloPrincipal =document.querySelector("#titulo-principal");
+const tituloPrincipal = document.querySelector("#titulo-principal");
 let botonesAgregar = document.querySelectorAll(".producto-agregar");
-const numerito=document.querySelector("#numerito")
+const numerito = document.querySelector("#numerito");
 
-
-
-
-function cargarProductos (productosElegidos) {
-    contenedorProductos.innerHTML ="";
+function cargarProductos(productosElegidos) {
+    contenedorProductos.innerHTML = "";
 
     productosElegidos.forEach(producto => {
         const div = document.createElement("div");
@@ -309,71 +308,55 @@ function cargarProductos (productosElegidos) {
             <img class="producto-imagen" src="${producto.imagen}" alt="${producto.titulo}">
             <div class="producto-detalles">
                 <h3 class="producto-titulo">${producto.titulo}</h3>
-                        <p class="producto-precio">${producto.precio}</p>
-                        <button class="producto-agregar" id="${producto.id}">Agregar</button>
-
+                <p class="producto-precio">${producto.precio}</p>
+                <button class="producto-agregar" id="${producto.id}">Agregar</button>
             </div>
-
-
         `;
         contenedorProductos.append(div);
-
-    })
+    });
     actualizarBotonesAgregar();
 }
 
 cargarProductos(productos);
 
-botonesCategorias.forEach(boton =>{
-    boton.addEventListener("click",(e) =>{
-
+botonesCategorias.forEach(boton => {
+    boton.addEventListener("click", (e) => {
         botonesCategorias.forEach(boton => boton.classList.remove("active"));
         e.currentTarget.classList.add("active");
-       
-        if(e.currentTarget.id !="todos"){
-            const productoCategoria = productos.find(producto =>producto.categoria.id ===e.currentTarget.id);
-            tituloPrincipal.innerText =productoCategoria.categoria.nombre;
-            const productosBoton =productos.filter (producto => producto.categoria.id === e.currentTarget.id);
-            cargarProductos(productosBoton); 
-        }else{
-            tituloPrincipal.innerText ="Todos los productos";
-            cargarProductos(productos); 
-        }
-  
-        
 
-    })
+        if (e.currentTarget.id != "todos") {
+            const productoCategoria = productos.find(producto => producto.categoria.id === e.currentTarget.id);
+            tituloPrincipal.innerText = productoCategoria.categoria.nombre;
+            const productosBoton = productos.filter(producto => producto.categoria.id === e.currentTarget.id);
+            cargarProductos(productosBoton);
+        } else {
+            tituloPrincipal.innerText = "Todos los productos";
+            cargarProductos(productos);
+        }
+    });
 });
-function actualizarBotonesAgregar(){
-    botonesAgregar = document.querySelectorAll("producto-agregar")
-    botonesAgregar.forEach(boton =>{
-        boton.addEventListener("click",agregarAlcarrito)
-    })
-    
+
+function actualizarBotonesAgregar() {
+    botonesAgregar = document.querySelectorAll(".producto-agregar");
+    botonesAgregar.forEach(boton => {
+        boton.addEventListener("click", agregarAlcarrito);
+    });
 }
+
 const productosenCarrito = [];
 
-function agregarAlcarrito() {
-
-        const idBoton = e.currentTarget.id;
-        const productoAgregado =productos.find(producto => producto.id ===idBoton);
-        if(productosenCarrito.some(producto => producto.id === idBoton)){
+function agregarAlcarrito(e) {
+    const idBoton = e.currentTarget.id;
+    const productoAgregado = productos.find(producto => producto.id === idBoton);
+    if (productosenCarrito.some(producto => producto.id === idBoton)) {
         const index = productosenCarrito.findIndex(producto => producto.id === idBoton);
         productosenCarrito[index].cantidad++;
-
-
-        }else {
-            productoAgregado.cantidad = 1;
-            productosenCarrito.push(productoAgregado);
-        }
-
-       
-        
-
-
+    } else {
+        productoAgregado.cantidad = 1;
+        productosenCarrito.push(productoAgregado);
+    }
 }
+
 function actualizarNumerito() {
-    let numerito =productosenCarrito.reduce((acc, producto)=> acc + producto.cantidad, 0);
-    
+    let numerito = productosenCarrito.reduce((acc, producto) => acc + producto.cantidad, 0);
 }
-console.log(botonesAgregar)
